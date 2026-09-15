48dcea852f238706b047e4f06fb3ff17279d1c07
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-15 20:57:19 +0530

    Added data for Navbar links

---
+++ data/navbarLinkData.data.ts
@@ -0,0 +1,8 @@
+import { NavbarLinkProps } from "@/types/navbarlink.types";
+
+export const NavbarLinksData: NavbarLinkProps[] = [
+    { id: 1, href: '/', name: 'Home' },
+    { id: 2, href: '/jobs', name: 'Jobs' },
+    { id: 3, href: '/docs', name: 'Docs' },
+    { id: 4, href: '/support', name: 'Support' },
+]
