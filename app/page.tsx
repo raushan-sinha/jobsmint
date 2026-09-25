f606965cef98946fa35018a34b88879d7dfa5c36
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-25 15:06:09 +0530

    Code added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { HeroSection } from "@/components/home";
 
-export default function Home() {
+export default async function Home() {
+    const response = await fetch('http://localhost:3000//api/jobs');
+    const result = await response.json();
+    console.log(result);
+    
     return (
         <>
             <HeroSection />
