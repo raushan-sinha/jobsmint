cd916057a190c01b5895ef0dc532a387ab3bd4cd
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-25 15:09:37 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ export default async function Home() {
     const response = await fetch('http://localhost:3000//api/jobs');
     const result = await response.json();
     console.log(result);
-    
+
     return (
-        <>
-            <HeroSection />
-        </>
+        <HeroSection />
     );
 }
