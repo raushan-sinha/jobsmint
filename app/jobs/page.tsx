1a714b3a4152e10b5e46ad75c1754a46770cb907
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-26 15:25:14 +0530

    Updated code

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,7 +1,7 @@
 export default async function JobsPage() {
     const response = await fetch('https://jobsmint.vercel.app/api/jobs');
     const result = await response.json();
-    console.log(result);
+    console.log(result.data.jobs);
 
     return (
         <h1>Jobs Page</h1>
