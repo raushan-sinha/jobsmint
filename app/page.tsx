348e0a3994a90aed465b8259c54a01477e9d2950
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-26 10:44:13 +0530

    API link updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { HeroSection } from "@/components/home";
 
 export default async function Home() {
-    const response = await fetch('https://jobsmint.vercel.app//api/jobs');
+    const response = await fetch('https://jobsmint.vercel.app/api/jobs');
     const result = await response.json();
     console.log(result);
 
