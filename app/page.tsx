f88e2babf7df8e6f3e5d14cc3942454e5c942b9f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-26 10:56:49 +0530

    Code removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import { HeroSection } from "@/components/home";
 
-export default async function Home() {
-    const response = await fetch('https://jobsmint.vercel.app/api/jobs');
-    const result = await response.json();
-    console.log(result);
-
+export default function Home() {
     return (
         <HeroSection />
     );
